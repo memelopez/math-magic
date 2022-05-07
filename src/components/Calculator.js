67951216214b3a6f19108ac3a4335e84fd2d819d
memelopez <meme30lopez@gmail.com>
2022-05-07 14:31:17 -0600

    Remove classname attribute from user created component <Calculator/>

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -57,24 +57,24 @@ class Calculator extends React.Component {
       <div className="calcContainer">
         <Output result={0} />
         <div className="keysGrid">
-          <Button className="calcBtn" value="AC" />
-          <Button className="calcBtn" value="+/-" />
-          <Button className="calcBtn" value="%" />
-          <Button className="calcBtn" value="+" />
-          <Button className="calcBtn" value="7" />
-          <Button className="calcBtn" value="8" />
-          <Button className="calcBtn" value="9" />
-          <Button className="calcBtn" value="x" />
-          <Button className="calcBtn" value="4" />
-          <Button className="calcBtn" value="5" />
-          <Button className="calcBtn" value="6" />
-          <Button className="calcBtn" value="-" />
-          <Button className="calcBtn" value="1" />
-          <Button className="calcBtn" value="2" />
-          <Button className="calcBtn" value="3" />
-          <Button className="calcBtn" value="+" />
-          <Button className="calcBtn" value="0" />
-          <Button className="calcBtn" value="." />
+          <Button value="+/-" />
+          <Button value="AC" />
+          <Button value="%" />
+          <Button value="+" />
+          <Button value="7" />
+          <Button value="8" />
+          <Button value="9" />
+          <Button value="x" />
+          <Button value="4" />
+          <Button value="5" />
+          <Button value="6" />
+          <Button value="-" />
+          <Button value="1" />
+          <Button value="2" />
+          <Button value="3" />
+          <Button value="+" />
+          <Button value="0" />
+          <Button value="." />
           <Button value="=" />
         </div>
       </div>

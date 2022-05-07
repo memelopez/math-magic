61522bfbf337a9784bffe76cf24c02d3017051f9
memelopez <meme30lopez@gmail.com>
2022-05-07 17:27:23 -0600

    Remove Button component to a class of its own and added funtions and props for onClick event

---
+++ src/components/Button.js
@@ -0,0 +1,44 @@
+// /src/components/Button.js
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Button extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    const { handleClick } = this.props;
+    handleClick(e.target.textContent);
+  }
+
+  render() {
+    const { value } = this.props;
+    let classes4btn = 'calcBtn';
+    if (value === '0') {
+      classes4btn = 'calcBtn span2';
+    }
+    if (value === '+' || value === 'x' || value === '-' || value === '=') {
+      classes4btn = 'calcBtn orangeBtn';
+    }
+    return (
+      <button className={classes4btn} type="button" onClick={this.handleClick}>
+        {value}
+      </button>
+    );
+  }
+}
+
+Button.defaultProps = {
+  value: '-1',
+  handleClick: () => {},
+};
+
+Button.propTypes = {
+  value: PropTypes.string,
+  handleClick: PropTypes.func,
+};
+
+export default Button;
